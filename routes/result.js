241047c2449acb3b079a980b219bc918515c6109
Jota Mario Miranda Guerra <jotayes101@gmail.com>
2020-05-24 17:18:57 -0500

    v1.0

--- routes/result.js
+++ routes/result.js
@@ -1,22 +1,21 @@
 ﻿import express from 'express';
 const router = express.Router();
-const fs = require('fs');
 const apiDrive = require("../helpers/driveHdl");
 
 // Agregar una nota
-router.get('/results', async(req, res) => {
+router.get('/results', async (req, res) => {
   try {
 
-    // Load client secrets from a local file.
-    fs.readFile('credentials.json', (err, content) => {
-      if (err) return console.log('Error loading client secret file:', err);
-      // Authorize a client with credentials, then call the Google Drive API.
-      apiDrive.authorize(JSON.parse(content), apiDrive.listFiles);
-    });
+    const dataFile = await apiDrive.getFileId(req.query);
+    // console.log('fileId: ', dataFile);
+    if (dataFile) {
+      res.status(200).json(dataFile);
+    } else {
+      res.status(404).json({
+        msg: 'No existe ningun resultado con esos datos, por favor verifique',
+      })
+    }
 
-    res.status(200).json({
-      "msg": "jota"
-    }); 
   } catch (error) {
     return res.status(400).json({
       mensaje: 'Ocurrio un error',
